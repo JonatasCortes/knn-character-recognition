3fce6fb811e4f4c937c4ff76acd8b0db540ac558
Jonatas <joncortes.on@gmail.com>
2026-08-02 14:56:57 -0300

    FEAT: add class to represent EMNIST dataset labels

---
+++ src/hnsw/domain/_label.py
@@ -0,0 +1,19 @@
+class Label(str):
+
+    _LABEL_MAP = {
+        0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8',
+        9: '9', 10: 'A', 11: 'B', 12: 'C', 13: 'D', 14: 'E', 15: 'F', 16: 'G',
+        17: 'H', 18: 'I', 19: 'J', 20: 'K', 21: 'L', 22: 'M', 23: 'N', 24: 'O',
+        25: 'P', 26: 'Q', 27: 'R', 28: 'S', 29: 'T', 30: 'U', 31: 'V', 32: 'W',
+        33: 'X', 34: 'Y', 35: 'Z', 36: 'a', 37: 'b', 38: 'd', 39: 'e', 40: 'g',
+        41: 'h', 42: 'n', 43: 'q', 44: 'r', 45: 't', 46: 'u'
+    }
+
+    def __new__(cls, label: int) -> None:
+        if label < 0 or label > 46:
+            raise ValueError("label must be an integer value between 0 and 46")
+        super().__new__(cls, cls._decode(label))
+
+    @classmethod
+    def _decode(cls, label: int) -> str:
+        return cls._LABEL_MAP[label]
