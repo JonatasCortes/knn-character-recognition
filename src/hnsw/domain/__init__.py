d1b0fb700a87ab7d803ae658c27a8ecf58d56139
Jonatas <joncortes.on@gmail.com>
2026-08-17 17:30:44 -0300

    DELETE: Layer class

--- src/hnsw/domain/__init__.py
+++ src/hnsw/domain/__init__.py
@@ -1,11 +1,9 @@
 from ._label import Label
 from ._position import Position
 from ._node import Node
-from ._layer import Layer
 
 __all__ = [
     "Label",
     "Position",
     "Node",
-    "Layer"
 ]
